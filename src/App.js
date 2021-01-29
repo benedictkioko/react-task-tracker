30644a03f38466858b3cfefff8ef8dc5cdb241c2
benedict <kiokobenedict@gmail.com>
2021-01-29 18:21:26 +0300

    added a global state for task data to be available project wide

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import './styles/index.css';
-
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
 
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Project Sync',
+        day: 'Feb 5th at 10:30 am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Meeting at work',
+        day: 'Feb 7th at 2:30 pm',
+        reminder: false,
+    }
+  ])
+
   return (
     <div className="container">
       < Header/>
-      < Tasks />
+      < Tasks tasks = { tasks }/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30 pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Project Sync',
-            day: 'Feb 5th at 10:30 am',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Meeting at work',
-            day: 'Feb 7th at 2:30 pm',
-            reminder: false,
-        }
-    ])
+const Tasks = ({tasks}) => {
     return (
         <>
           {tasks.map((task) => (

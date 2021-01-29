42dcd5d4ebaebba8586d3139ac41d2f7ce618b64
benedict <kiokobenedict@gmail.com>
2021-01-30 00:05:47 +0300

    added a toggle reminder to update data on the server

--- src/App.js
+++ src/App.js
@@ -19,9 +19,18 @@ function App() {
 
   }, [])
 
-  // Fetch Taks
+  // Fetch Tasks
   const fetchTasks = async () =>{
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+
+  }
+
+   // Fetch Task
+   const fetchTask = async (id) =>{
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data
@@ -60,10 +69,26 @@ function App() {
 
 
   // Toggle Reminder
-  const toggleReminder = (id) =>{
+  const toggleReminder = async(id) =>{
+
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle, reminder : !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+
+      method: 'PUT',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(updatedTask),
+      
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) =>
-        task.id ===id ? {...task, reminder: !task.reminder}:task
+        task.id ===id ? {...task, reminder: data.reminder}:task
       )
     )
   }

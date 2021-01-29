c9dde05427f6e503c21050667c838226ab524ff2
benedict <kiokobenedict@gmail.com>
2021-01-29 18:13:34 +0300

    added State and useState Hook to Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,26 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Project Sync',
-        day: 'Feb 5th at 10:30 am',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Meeting at work',
-        day: 'Feb 7th at 2:30 pm',
-        reminder: false,
-    }
-]
+import { useState } from 'react'
+
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30 pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Project Sync',
+            day: 'Feb 5th at 10:30 am',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Meeting at work',
+            day: 'Feb 7th at 2:30 pm',
+            reminder: false,
+        }
+    ])
     return (
         <>
           {tasks.map((task) => (

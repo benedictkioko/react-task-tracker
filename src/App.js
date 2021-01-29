906f2eb4743e3fa53d471328fb9b5ae5839ea9d3
benedict <kiokobenedict@gmail.com>
2021-01-29 18:06:02 +0300

    added a task component with .map() list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/index.css';
 
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
 
   return (
     <div className="container">
-      <Header/>
+      < Header/>
+      < Tasks />
     </div>
   );
 }

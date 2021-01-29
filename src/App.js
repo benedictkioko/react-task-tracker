32fa4f23705a3aac98000febf4247ebdabb30b38
benedict <kiokobenedict@gmail.com>
2021-01-29 17:23:54 +0300

    removed tailwind css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header'
 function App() {
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+    <div className="header">
       <Header/>
     </div>
   );

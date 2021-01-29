5fb35af6cf465b18e8ec45698a5b5de963108a55
benedict <kiokobenedict@gmail.com>
2021-01-30 00:35:31 +0300

    added router, footer and about components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 function App() {
 
@@ -94,19 +97,28 @@ function App() {
   }
 
   return (
-    <div className="container">
-      < Header onAdd = {()=> setshowAddTask (!showAddTask)}
-        showAdd = {showAddTask}
-      />
-      {showAddTask && <AddTask onAdd = {addTask} />}
-      { tasks.length > 0 ?(
-        < Tasks tasks = { tasks } onDelete={deleteTask}
-          onToggle={toggleReminder}
+    <Router>
+      <div className="container">
+        < Header onAdd = {()=> setshowAddTask (!showAddTask)}
+          showAdd = {showAddTask}
         />
-      ):(
-        'No Tasks Available'
-      )}
-    </div>
+       
+        <Route path='/' exact render={(props)=>(
+          <>
+            {showAddTask && <AddTask onAdd = {addTask} />}
+            { tasks.length > 0 ?(
+              < Tasks tasks = { tasks } onDelete={deleteTask}
+                onToggle={toggleReminder}
+              />
+            ):(
+              'No Tasks Available'
+            )}
+          </>
+        )}/>
+        <Route path='/about' component={About}/>
+        < Footer />
+      </div>
+    </Router>
   );
 }
 
